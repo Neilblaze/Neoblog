d584c127d48e42bcefe978b1e80e4ef0a6d718e9
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-10-24 21:26:20 +0530

    root config

---
+++ gatsby-config.js
@@ -0,0 +1,80 @@
+module.exports = {
+  siteMetadata: {
+    title: "NΞILBLAZΞ's Blog",
+    description: `A place to share my thoughts.`,
+    author: `Pratyay Banerjee`,
+  },plugins: [
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-highlight-code`
+          },
+        ],
+      },
+    },
+  ],
+  plugins: [
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-responsive-image`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ]
+      }
+    }
+  ],
+  plugins: [
+    `gatsby-plugin-react-helmet`, //Can also use '' but `` are usually Gatbsy standard, as it also allows  using JS expression
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,// The plugin that converts the markdown files into web pages
+      options: {
+        plugins: [
+          `gatsby-remark-reading-time`,
+          // ...
+        ],
+      },
+    },
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#d9fcff`,
+        theme_color: `#d9fcff`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
