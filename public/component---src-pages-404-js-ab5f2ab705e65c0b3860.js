a7e2481771d382c5d2733785593eaa361657334b
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-10-24 22:48:28 +0530

    public build

---
+++ public/component---src-pages-404-js-ab5f2ab705e65c0b3860.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),u=n.n(a),l=n("Kvkj");t.default=function(){return u.a.createElement(l.e,null,u.a.createElement(l.j,{title:"404: Not found"}),u.a.createElement("h1",null,"404: Not Found"),u.a.createElement("p",null,"You just hit a route that doesn't exist... :("))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-ab5f2ab705e65c0b3860.js.map
---
+++ public/component---src-pages-page-2-js-946d4d78512ea5f35400.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{p5nM:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),l=n("Wbzz"),c=n("Kvkj");t.default=function(){return o.a.createElement(c.e,null,o.a.createElement(c.j,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-946d4d78512ea5f35400.js.map
