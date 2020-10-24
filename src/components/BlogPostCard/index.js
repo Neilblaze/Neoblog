6bec3c95f486edf633324b2f648120011602c251
Pratyay Banerjee <48355572+Neilblaze@users.noreply.github.com>
2020-10-24 21:30:31 +0530

    Blogpost card

---
+++ src/components/BlogPostCard/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Img from 'gatsby-image'
+import { Link } from 'gatsby'
+// Icons
+import DateIcon from 'images/calendar.svg'
+import TimeIcon from 'images/watch.svg'
+// Styles
+import {
+    CardWrapper,
+    ImgWrapper,
+    TextWrapper,
+    StatWrapper,
+    DateWrapper,
+    ReadingTimeWrapper
+} from './BlogPostCard.styles'
+
+import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader';
+deckDeckGoHighlightElement();
+
+const BlogPostCard = ({ slug, title, date, readingTime, excerpt, image }) => (
+        <CardWrapper>
+            <ImgWrapper>
+                <Img fluid={image} />
+            </ImgWrapper>
+            <TextWrapper>
+                <Link to={slug}>
+                    <h2>{title}</h2>
+                </Link>
+                <StatWrapper>
+                    <DateWrapper>
+                        <img src={DateIcon} alt="date"/>
+                        {date}
+                    </DateWrapper>
+                    <ReadingTimeWrapper>
+                        <img src={TimeIcon} alt="time"/>
+                        {readingTime}
+                    </ReadingTimeWrapper>
+                </StatWrapper>
+                <p>{excerpt}</p>
+            </TextWrapper>  
+        </CardWrapper>
+    )
+
+
+export default BlogPostCard
